738df0c934fb31a0b95ad92b0f23f12e13a77b0e
tuongmaimanh <maituong888@gmail.com>
2022-07-28 09:55:27 +0700

    commit1

--- app.js
+++ app.js
@@ -2,7 +2,19 @@ const http = require('http')
 
 const server = http.createServer((req,res) => {
     console.log(req.url,req.method,req.headers)
-
+    const url = req.url
+    if(url == '/'){
+        res.write(`<html>
+    <header></header>
+    <body>
+        <form action="/message" method="POST">
+            <input type='text'>
+            <button type='submit'>Send</button>
+        </form>
+    </body>
+    </html>`)
+    return res.end()
+    }
     res.setHeader('Content-Type','text/html')
     res.write(`<html>
     <header>My first Page</header>
