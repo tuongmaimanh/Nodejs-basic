141aa3681e31214c9231881cda86fac36a7b7dff
tuongmaimanh <maituong888@gmail.com>
2022-07-31 14:40:36 +0700

    commit1

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express()
     res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Submit</button></form>')
     // next()
 })
-app.use('/product',(req,res,next)=>{
+app.post('/product',(req,res,next)=>{
     console.log(req.body)
     res.redirect('/')
 })
