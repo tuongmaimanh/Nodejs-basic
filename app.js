de2714f8bee44f764b640209bbfe97419b78b0e9
tuongmaimanh <maituong888@gmail.com>
2022-07-31 14:30:09 +0700

    commit1

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-const routes = require('./routes')
-const http = require('http');
 
-const server = http.createServer(routes.handler)
+const express = require('express')
+const bodyParser = require('body-parser')
 
-server.listen(3000);
+const app = express()
+
+ app.use(bodyParser.urlencoded({extended:false}))
+ app.use('/add-products',(req,res,next) => {
+    console.log('another middleware')
+    res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Submit</button></form>')
+    // next()
+})
+app.use('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/')
+})
+app.use('/',(req,res,next) => {
+    console.log('INDEX2')
+    res.send('<h1>index2</h1>')
+})
+
+app.listen(8000);
