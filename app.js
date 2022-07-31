e96a828ba3dbf5be7cb3e04088ee77b33c4a3375
tuongmaimanh <maituong888@gmail.com>
2022-07-31 15:29:32 +0700

    commit1

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const shopRoutes = require('./routes/shop')
 const app = express()
 
  app.use(bodyParser.urlencoded({extended:false}))
- app.use(adminRoutes)
+ app.use('/admin',adminRoutes)
  app.use(shopRoutes)
  app.use((req,res,next)=>{
     res.status(404).send('<h1>Page not Found (404)</h1>')
