bdb0479487103e425ade183ba797d0fab6ca1dca
tuongmaimanh <maituong888@gmail.com>
2022-07-31 15:21:57 +0700

    commit1

--- app.js
+++ app.js
@@ -8,5 +8,8 @@ const app = express()
  app.use(bodyParser.urlencoded({extended:false}))
  app.use(adminRoutes)
  app.use(shopRoutes)
+ app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not Found (404)</h1>')
+ })
 
 app.listen(8000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-router.use('/',(req,res,next) => {
+router.get('/',(req,res,next) => {
     console.log('INDEX2')
     res.send('<h1>index2</h1>')
+    next()
 })
 
 module.exports = router
