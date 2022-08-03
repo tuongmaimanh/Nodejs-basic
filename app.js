fad87b2b37c4d9f75b397f1456e1d2ee5c4c5038
tuongmaimanh <maituong888@gmail.com>
2022-08-03 09:25:55 +0700

    add layout with  Handlebars

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const path = require('path')
+const expressHbs = require("express-handlebars");
 
-const expressHbs = require('express-handlebars')
+const app = express();
 
+app.engine(
+  "hbs",
+  expressHbs({
+    layoutsDir: "views/layouts/",
+    defaultLayout: "main-layout",
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", "views");
 
-const app = express()
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.engine('hbs', expressHbs)
-app.set('view engine','pug')
-app.set('views','views')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/admin", adminData.routes);
+app.use(shopRoutes);
 
-
-const adminData = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
-
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(express.static(path.join(__dirname,'public')))
- app.use('/admin',adminData.routes)
- app.use(shopRoutes)
-
- app.use((req,res,next)=>{
-    res.status(404).render('404',{pageTitle:'Page not Found'})
- })
+app.use((req, res, next) => {
+  res.status(404).render("404", { pageTitle: "Page not Found" });
+});
 
 app.listen(8000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,21 @@
-const express = require('express')
-const patch = require('path')
-const router = express.Router()
-const rootDir = require('../util/path')
-const adminData = require('./admin')
+const express = require("express");
+const patch = require("path");
+const router = express.Router();
+const rootDir = require("../util/path");
+const adminData = require("./admin");
 
-router.get('/',(req,res,next) => {
-    const products = adminData.products
-    console.log(products)
-    res.render('shop',{products:products,pageTitle:'Shop',path:'/', hasProduct: products.length > 0})
-})
+router.get("/", (req, res, next) => {
+  const products = adminData.products;
+  console.log(products);
+  res.render("shop", {
+    products: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProduct: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+    layout: false,
+  });
+});
 
-module.exports = router
+module.exports = router;
